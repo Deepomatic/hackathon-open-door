60f98493961d8f259ed8132749f1475820ce08ef
Vincent Delaitre <vincent@deepomatic.com>
2016-08-26 14:45:56 +0000

    fixed giphy

--- api/api/views.py
+++ api/api/views.py
@@ -2,11 +2,8 @@ from django.template import loader
 from django.http import HttpResponse
 import os
 
-# when server restarts, we make sure the door is closed by calling the script
-os.system("open-door &")
-
 def index(request):
-    os.system("open-door &")
+    os.spawnlp(os.P_NOWAIT, "open-door")
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
 
